44d7cfdddfe7ca0fd8b77e7e9435b628a2dc10c7
polscode <paulsegales@gmail.com>
2024-07-06 20:16:40 -0500

    Instalar Tailwind CSS para estilos

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
+const { endOfLine } = require("./.prettierrc")
+
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "lf"
+      }
+    ]
+  }
 };
